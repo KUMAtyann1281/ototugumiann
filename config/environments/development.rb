118ed7bfb103b2cb67f126266307d2d000f22b26
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2024-11-23 04:06:44 +0000

    [Add] ユーザー側ユーザー機能の基盤完成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
+
   config.hosts << "c65accd3e8dc49d8995daebff03145f3.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- db/migrate/20241112062902_devise_create_users.rb
+++ db/migrate/20241112062902_devise_create_users.rb
@@ -4,10 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false, default: ""
+      t.string :name
+      t.text :information
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.text :information,          null: false, default: ""
       t.boolean :is_deleted,        null: false, default: false
       t.boolean :admin,             null: false, default: false
 
