7a26436364f52a436fa3a1f27a7795f6c4a37b5d
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2024-11-23 05:24:13 +0000

    [Add] ユーザー側ユーザー機能の調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   
   has_one_attached :profile_image
 
+  validates :name,
+    presence: true,
+    length: { minimum: 2, maximum: 10 },
+    uniqueness: true
+
+  validates :introduction,
+    length: { maximum: 50 }
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
