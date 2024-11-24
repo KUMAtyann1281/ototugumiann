dc7dc8aac6834c7ea10a208f09cc61b729b0e18d
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2024-11-24 16:42:05 +0000

    [Add] 実装フェーズ1完了

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,77 @@
 class Public::PostsController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      flash[:notice] = "You have created post successfully."
+      redirect_to post_path(@post.id)
+    else
+      flash.discard(:notice)
+      @posts = Post.all
+      render :index
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id), notice: "You have updated post successfully."
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post. destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def save_tag(sent_tags)
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+
+    old_tags.each do |old|
+      self.tags.delete Tag.find_by(name: old)
+    end
+
+    new_tags.each do |new|
+      new_post_tag = Tag.find_or_create_by(name: new)
+      self.tags << new_post_tag
+    end
+  end
+
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    @user = @post.user
+    unless @user.id == current_user.id
+      redirect_to posts_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
       resource :favorites, only: [:create, :destroy]
       resource :relationships, only: [:create, :destroy]
     end
-    resources :posts, only: [:new,:index,:show,:edit,:create,:destroy,:update] do
+    resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
       resource :favorite, only: [:create, :destroy]
       resources :comments, only: [:create, :destroy]
     end
