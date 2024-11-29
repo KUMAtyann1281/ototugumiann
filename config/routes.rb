592fb717c10aa44b95006d913517285b55c44d7e
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2024-11-29 08:47:04 +0000

    [Add] 管理者側のcontroller作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # URL /users/sign_in ...
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
-    sessions: 'public/sessions'
+    sessions: "public/sessions"
   }
 
   scope module: :public do
@@ -22,12 +22,11 @@ Rails.application.routes.draw do
       resource :favorite, only: [:create, :destroy]
       resources :comments, only: [:create, :destroy]
     end
-    resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    resources :groups, only: [:new, :index, :show, :create, :edit, :update, :destroy] do
       resource :group_users, only: [:create, :destroy]
       resources :chats, only: [:create, :destroy]
     end
   end
-
 
   # 管理者用
   # URL /admin/sign_in ...
@@ -36,8 +35,15 @@ Rails.application.routes.draw do
   }
   
   namespace :admin do
-    get 'homes/top'
-    resources :notices, only: [:new,:index,:show,:edit]
+    get "/" => "homes/top"
+    resources :notices, only: [:new, :index, :show, :edit, :update, :destroy]
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :posts, only: [:index, :show, :edit, :update, :destroy] do
+      resources :comments, only: [:destroy]
+    end
+    resources :groups, only: [:index, :show, :edit, :update, :destroy] do
+      resources :chats, only: [:destroy]
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
